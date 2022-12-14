77ab4a6d2492c15a32d7bd50388da398a8e712de
gstv1478 <gstv1478@gmail.com>
2022-12-14 18:36:36 -0300

    primeiro commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+
+import Container from './Container'
+
+import styles from './Navbar.module.css'
+import logo from '../img/costs_logo.png'
+
+function Navbar() {
+    return(
+      <nav class={styles.navbar}>
+            <Container>
+                <Link to="/"><img src={logo} alt="Logo da Costs"/></Link>
+                <ul class={styles.list}>
+                    <li className={styles.item}><Link to="/">Home</Link></li>
+                    <li className={styles.item}><Link to="/projects">Projetos</Link></li>
+                    <li className={styles.item}><Link to="/company">Empresa</Link></li>
+                    <li className={styles.item}><Link to="/contact">Contato</Link></li>
+                </ul>
+
+            </Container>
+      </nav>
+    ) 
+}
+
+export default Navbar
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,15 @@
+import ProjectForm from "../project/ProjectForm"
+
+import styles from './NewProject.module.css'
+
+function NewProject() {
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm />
+        </div>
+    )
+}
+
+export default NewProject
